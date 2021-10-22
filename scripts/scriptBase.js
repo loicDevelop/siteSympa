7c74901bb0b3cb3bf2ee3146b0aa8883b973ec10
YLoic la street <loic.crbn@outlook.com>
2021-10-22 17:13:42 +0200

    exo javascript

--- scripts/scriptBase.js
+++ scripts/scriptBase.js
@@ -1,17 +1,36 @@
-let h1 = document.querySelector("h1");
-console.log(h1)
-
-// id de balise -> nom
-let input = document.querySelector("#nom");
-console.log(input.value);
-
-// Image...
-let image = document.querySelector("#imgAChanger")
-image.src ="nouelle image"
-
 // Button du formulaire d'inscription
-function boom()
-{
+function boom() {
     let button = document.querySelector("#validerInscription");
     button.textContent = "Demande envoyé ...";
+}
+
+// Rouletet de meme
+function memeMove() {
+    let button = document.querySelector("#memeRoulette");
+    let img1 = document.querySelector("#meme1");
+    let img2 = document.querySelector("#meme2");
+    let img3 = document.querySelector("#meme3");
+    let img4 = document.querySelector("#meme4");
+    let prems = img1.src;
+    img1.src = img3.src;
+    img3.src = img4.src;
+    img4.src = img2.src;
+    img2.src = prems;
+}
+
+//calculatrice 
+var res ='' ;
+function calcul(variable) {
+    if (variable == 'res') {
+        res = '';
+    } else {
+        if(variable == '='){
+            res = eval(res);
+        }else {
+            res += variable
+        }
+    }
+    console.log('ajout de '+variable)
+    console.log(res);
+    document.querySelector('#res').textContent = res;     
 }
